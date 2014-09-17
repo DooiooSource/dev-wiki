decfcc704888fcd87c6d3b01b4aa445d7f15e86f
welkang <welkang@qq.com>
2014-09-17 16:13:27 +0800

    首页调整

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -3,7 +3,7 @@
 angular.module('dwikiApp')
     .controller('MainCtrl', function ($scope, $rootScope, $http, $routeParams) {
         $http.get('/api/articles').success(function (data) {
-            $scope.articles = data.articles;
+            $scope.articles = data.articles.splice(0, 10);
         });
 
         $http.get('http://dui.dooioo.com/public/demonew/main.json').success(function(data){
