ad46b4652fa3a49437be6b07f70db1ea23ec0f5a
lywzx <liu88yang889@163.com>
2015-03-09 18:18:46 +0800

    1、内容更新提交

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -6,10 +6,7 @@ angular.module('dwikiApp')
             $scope.articles = data.articles.splice(0, 10);
         });
 
-        /*$http.get('http://dui.dooioo.com/public/demonew/main.json').success(function(data){
-            $scope.docs = data;
-        });*/
-        $http.get('http://10.8.204.104/workspace/dui/doc/main.json').success(function(data){
+        $http.get('http://dui.dooioo.com/public/doc/main.json').success(function(data){
             $scope.docs = data;
         });
 
